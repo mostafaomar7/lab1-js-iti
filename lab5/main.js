a0d44a18c11500fcd35a181298c2db66ab6ef97c
unknown <amrm68853@gmail.com>
2025-05-04 19:33:43 +0200

    second commit

--- lab5/main.js
+++ lab5/main.js
@@ -1,74 +1,41 @@
-// Abstract Engine Class
-class Engine {
-    static #count = 0;
-  
-    constructor(source) {
-      if (new.target === Engine) {
-        throw new Error("Cannot abstract class Engine directly.");
+class Car {
+  constructor(x, y, elementId) {
+      this.x = x;
+      this.y = y;
+      this.element = document.getElementById(elementId);
+      this.interval = null;
+      this.updatePosition();
+  }
+
+  updatePosition() {
+      this.element.style.left = this.x + 'px';
+      this.element.style.top = this.y + 'px';
+  }
+
+  moveLeft() {
+      this.x -= 10;
+      this.updatePosition();
+  }
+
+  moveRight() {
+      this.x += 10;
+      this.updatePosition();
+  }
+
+  moveCar(direction) {
+      this.stopMoving();
+
+      if (direction === 'left') {
+          this.interval = setInterval(() => this.moveLeft(), 100);
+      } else if (direction === 'right') {
+          this.interval = setInterval(() => this.moveRight(), 100);
       }
-      this.source = source;
-      Engine.#count++;
-    }
-  
-    static getCount() {
-      return Engine.#count;
-    }
   }
-  
-  // Car Class
-  class Car extends Engine {
-    constructor(top, left, source) {
-      super(source);
-      this.top = top;
-      this.left = left;
-  
-      // Create car image element
-      this.img = document.createElement("img");
-      this.img.src = this.source;
-      this.img.style.position = "absolute";
-      this.img.style.top = `${this.top}px`;
-      this.img.style.left = `${this.left}px`;
-      this.img.style.width = "100px";
-      document.body.appendChild(this.img);
-    }
-  
-    set Top(value) {
-      this.top = value;
-      this.img.style.top = `${this.top}px`;
-    }
-  
-    set Left(value) {
-      this.left = value;
-      this.img.style.left = `${this.left}px`;
-    }
-  
-    moveLeft(step = 10) {
-      this.left = Math.max(0, this.left - step);
-      this.img.style.left = `${this.left}px`;
-    }
-  
-    moveRight(step = 10) {
-      const pageWidth = window.innerWidth;
-      const maxRight = pageWidth - this.img.offsetWidth;
-      this.left = Math.min(maxRight, this.left + step);
-      this.img.style.left = `${this.left}px`;
-    }
-  
-    changeStyle(styleObj) {
-      Object.assign(this.img.style, styleObj);
-    }
-  
-    moveCar(direction) {
-      const move = () => {
-        if (direction === "left" && this.left > 0) {
-          this.moveLeft();
-          requestAnimationFrame(move);
-        } else if (direction === "right" && this.left < window.innerWidth - this.img.offsetWidth) {
-          this.moveRight();
-          requestAnimationFrame(move);
-        }
-      };
-      move();
-    }
+
+  stopMoving() {
+      if (this.interval) {
+          clearInterval(this.interval);
+          this.interval = null;
+      }
   }
-  
+}
